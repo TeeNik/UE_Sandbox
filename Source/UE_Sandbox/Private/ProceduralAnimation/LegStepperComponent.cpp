b8509fd4abef4b4dba593b442b8869ac03f35f48
TeeNik <teenik3315@gmail.com>
2022-07-04 00:15:10 +0300

    leg stepper refactor

--- Source/UE_Sandbox/Private/ProceduralAnimation/LegStepperComponent.cpp
+++ Source/UE_Sandbox/Private/ProceduralAnimation/LegStepperComponent.cpp
@@ -1,10 +1,11 @@
 #include "ProceduralAnimation/LegStepperComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 ULegStepperComponent::ULegStepperComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 
-	WantStepAtDistance = 120;
+	WantStepAtDistance = 300.0f;
 	HomeTransform = FVector(0, 120, 0);
 	StartEndPoint = HomeTransform;
 	StepOvershootFraction = 0.5f;
@@ -22,38 +23,46 @@ void ULegStepperComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	const FTransform ownerTransfrom = GetOwner()->GetTransform();
-	const FVector worldHomePos = ownerTransfrom.TransformPosition(HomeTransform);
-
-
-	const FVector homePos = FVector::VectorPlaneProject(worldHomePos, FVector::UpVector);
-	const FVector endPos = FVector::VectorPlaneProject(EndPoint, FVector::UpVector);
-	FVector diff = endPos - homePos;
-
-	const float sqrDist = (diff).SquaredLength();
-	diff.Normalize();
-
-	if(sqrDist > WantStepAtDistance * WantStepAtDistance)
+	if (!IsFarFromPoint)
 	{
-		const FVector forward = GetOwner()->GetActorForwardVector();
-		const float overshootDistance = WantStepAtDistance * StepOvershootFraction;
-		const FVector overshootVector = forward * overshootDistance;
+		const FTransform ownerTransfrom = GetOwner()->GetTransform();
+		const FVector worldHomePos = ownerTransfrom.TransformPosition(HomeTransform);
 
-		FHitResult hit;
-		FVector rayOrigin = worldHomePos + overshootVector + FVector::UpVector * 200.0f;
-		FVector rayEnd = rayOrigin - FVector::UpVector * 1000.0f;
-		bool result = GetWorld()->LineTraceSingleByChannel(hit, rayOrigin, rayEnd, ECollisionChannel::ECC_WorldStatic);
-		if (result)
+		const FVector homePos = FVector::VectorPlaneProject(worldHomePos, FVector::UpVector);
+		const FVector endPos = FVector::VectorPlaneProject(EndPoint, FVector::UpVector);
+		FVector diff = endPos - homePos;
+
+		const float sqrDist = (diff).SquaredLength();
+		diff.Normalize();
+
+		if (sqrDist > WantStepAtDistance * WantStepAtDistance)
 		{
-			DrawDebugSphere(GetWorld(), hit.ImpactPoint, 5.0f, 12, FColor::Yellow, false, 10, 10, 2.5f);
+			IsFarFromPoint = true;
 		}
-		EndPoint = worldHomePos + overshootVector;
+		DrawDebugSphere(GetWorld(), EndPoint, 5.0f, 12, FColor::Red, false, -1, 10, 2.5f);
 	}
-	
-	//DrawDebugLine(GetWorld(), endPos, homePos, FColor::Blue, false, -1, 10, 2.5f);
-	DrawDebugLine(GetWorld(), endPos, homePos, FColor::Blue, false, -1, 10, 2.5f);
-	DrawDebugSphere(GetWorld(), EndPoint, 5.0f, 12, FColor::Red, false, -1, 10, 2.5f);
-	DrawDebugSphere(GetWorld(), worldHomePos, 5.0f, 12, FColor::Green, false, -1, 10, 2.5f);
+}
+
+FVector ULegStepperComponent::GetRandomPointInRadius() const
+{
+	const float angle = FMath::RandRange(0.0f, 360.0f);
+	const FVector up = GetOwner()->GetActorUpVector();
+	FVector dir = GetOwner()->GetActorForwardVector();
+	dir = UKismetMathLibrary::RotateAngleAxis(dir, angle, up);
+	UE_LOG(LogTemp, Log, TEXT("GetRandomPointInRadius %f %s"), angle, *up.ToString());
+
+	const float radius = FMath::RandRange(200, 300);
+
+	FHitResult hit;
+	FVector rayOrigin = GetOwner()->GetActorLocation() + dir * radius + FVector::UpVector * 200.0f;
+	FVector rayEnd = rayOrigin - FVector::UpVector * 1000.0f;
+	bool result = GetWorld()->LineTraceSingleByChannel(hit, rayOrigin, rayEnd, ECollisionChannel::ECC_WorldStatic);
+	if (result)
+	{
+		DrawDebugSphere(GetWorld(), hit.ImpactPoint, 5.0f, 12, FColor::Magenta, false, 5, 10, 2.5f);
+	}
+
+	return hit.ImpactPoint;
 }
 
 FVector ULegStepperComponent::GetEndPoint() const
@@ -67,5 +76,16 @@ FVector ULegStepperComponent::GetEndPoint() const
 		return hit.ImpactPoint;
 	}
 	return EndPoint;
+}
+
+void ULegStepperComponent::GetNewPoint()
+{
+	EndPoint = GetRandomPointInRadius();
+	IsFarFromPoint = false;
+}
+
+bool ULegStepperComponent::GetIsFarFromPoint() const
+{
+	return IsFarFromPoint;
 }
 
--- Source/UE_Sandbox/Public/ProceduralAnimation/LegStepperComponent.h
+++ Source/UE_Sandbox/Public/ProceduralAnimation/LegStepperComponent.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -17,6 +15,9 @@ public:
 
 	FVector GetEndPoint() const;
 
+	void GetNewPoint();
+	bool GetIsFarFromPoint() const;
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -33,5 +34,8 @@ protected:
 
 private:
 	FVector EndPoint;
+	FVector GetRandomPointInRadius() const;
+
+	bool IsFarFromPoint = false;
 
 };
